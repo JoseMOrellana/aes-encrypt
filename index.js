b69a15e9e090445f774e7c0732364f013e4b27f4
José O <jose.mom.1304@gmail.com>
2020-03-08 00:54:49 -0430

    first commit

---
+++ index.js
@@ -0,0 +1,216 @@
+ //Input related elements
+const stringInputBox = document.querySelector('#input-string')
+const stringIcon = document.querySelector('#icon-span')
+const cypherKeyInputArea = document.querySelector('#cypher-string-area')
+const cypherInputBox = document.querySelector('#cypher-string')
+const cypherIcon = document.querySelector('#cypher-icon-span')
+const warningText = document.querySelector('#warning-text')
+const submitButton = document.querySelector('#submit-button')
+const resetButton = document.querySelector('#reset-button')
+
+//Output related elements
+const firstStateTable = document.querySelector('#first-state-table')
+const cypherTable = document.querySelector('#cypher-table')
+const resultOutput = document.querySelector('#result-output')
+const resultMatrixesArea = document.querySelector('#results-area')
+const resultButtons = document.querySelectorAll('.result-toggle')
+const allArea = document.querySelector('#all-area');
+
+const progressBar = document.querySelector('#progress-bar')
+
+const resultButtonsArray = Array.from(resultButtons)
+
+//Adding event listeners to the elements who serve as toggle buttons for the results
+resultButtonsArray.map(resultButton => {
+	resultButton.addEventListener('click', function (event) {
+		replaceElementWithClass('selected', this)
+		
+		const area = getArea(this.getAttribute('id'));
+
+		replaceElementWithClass('show', area )
+	})
+})
+
+//Adding event listeners to both text input box
+stringInputBox.addEventListener('keyup', (e) => {
+	if (stringInputBox.value.length !== 16) {
+		inputIsInvalid( stringInputBox, stringIcon)
+		cypherKeyInputArea.style.display = "none"
+	} else {
+		inputIsValid(stringInputBox, stringIcon )
+		cypherKeyInputArea.style.display = "block"
+		if (cypherInputBox.value.length == 16) {
+			submitButton.disabled = false
+		}
+	}
+})
+
+cypherInputBox.addEventListener('keyup', (e) => {
+	if (cypherInputBox.value.length !== 16) {
+		inputIsInvalid( cypherInputBox, cypherIcon)
+	} else {
+		inputIsValid( cypherInputBox, cypherIcon)
+		submitButton.disabled = false
+	}	
+})
+
+//For resetting all inputs as well as hiding all previous result related elements
+resetButton.addEventListener('click', () => {
+	
+	resetInputs()
+	resetTables()
+	resultOutput.innerHTML = ""
+	resultMatrixesArea.style.display = "none"
+})
+
+//Logic for the AES encryption algorithm, triggered when an user submits input info
+submitButton.addEventListener('click', (e) => {
+	const N = 10
+
+	e.preventDefault()
+
+	if ( allArea.childElementCount === 0) {
+		createTables()	
+	}
+
+	const { strMatrix, cypherKeyMatrix, roundResultMatrixes, subBytesMatrixes,
+			shiftedRowsMatrixes, columnMixedMatrixes, subkeysMatrixes, result} = cypher( stringInputBox.value, cypherInputBox.value, N)
+
+	printMatrix(strMatrix, "state")
+	printMatrix(cypherKeyMatrix, "key")
+
+	printAllResults(roundResultMatrixes, 'round-result')
+	printAllResults(subBytesMatrixes, 'sub-bytes')
+	printAllResults(shiftedRowsMatrixes, 'shift-row')
+	printAllResults(columnMixedMatrixes, 'mix-column')
+	printAllResults(subkeysMatrixes, 'sub-keys')
+
+	resultOutput.innerHTML = result
+	
+	displayResultButtons()
+	allArea.classList.add('show');
+	resultMatrixesArea.style.display = "block"
+
+	progressBar.style.width = "0";
+})
+
+//Function that replaces which element has a given class name
+const replaceElementWithClass = ( className, next ) => {
+	const current = document.querySelector("." + className)
+	current.classList.remove(className)
+	next.classList.add(className)
+}
+
+const getArea = ( id ) => {
+	return document.querySelector('#' + id + '-area')
+}
+
+//Function that display visual cues for when the input given by an user is STILL INVALID
+const inputIsInvalid = ( input, inputIcon ) => {
+	submitButton.disabled = true
+	input.classList.remove('valid-input-box')
+	warningText.innerHTML = "(La cadena debe tener exactamente 16 caracteres)"
+	inputIcon.innerHTML = "<i class='fas fa-times-circle invalid-icon-color'></i>"
+}
+
+//Function that display visual cues for when the input given by an user is FINALLY VALID
+const inputIsValid = ( input, inputIcon ) => {
+	input.classList.add('valid-input-box')
+	inputIcon.innerHTML = "<i class='fas fa-check-circle valid-icon-color'></i>"
+	warningText.innerHTML = ""
+}
+
+const resetInputs = () => {
+	stringInputBox.value = ""
+	stringIcon.innerHTML = "<i class='fas fa-times-circle invalid-icon-color'></i>"
+	stringInputBox.className = "invalid-input-box"
+
+	cypherKeyInputArea.style.display = "none"
+	cypherInputBox.value = ""
+	cypherInputBox.className = "invalid-input-box"
+	cypherIcon.innerHTML = ""
+
+	submitButton.disabled = true
+
+	warningText.innerHTML = "(La cadena debe tener exactamente 16 caracteres)"
+}
+
+const resetTables = () => {
+	for (let i = 1; i < 5; i++) {
+		for (let j = 0; j < 4; j++) {
+			firstStateTable.rows[i].cells[j].innerHTML = ""
+			cypherTable.rows[i].cells[j].innerHTML = ""
+		}
+	}
+}
+
+const createTables = () => {
+	const tableConfig = [
+		{
+			title: 'Round',
+			idPrefix: 'round-result'
+		},
+		{
+			title: 'Sub Bytes',
+			idPrefix: 'sub-bytes'
+		},
+		{
+			title: 'Shift Rows',
+			idPrefix: 'shift-row'
+		},
+		{
+			title: 'Mix Column',
+			idPrefix: 'mix-column'
+		},
+		{
+			title: 'Sub Key',
+			idPrefix: 'sub-keys'
+		},
+	]
+
+	for (let i = 0; i < 10; i++) {
+		tableConfig.forEach(config => {
+			const boxId = config.idPrefix + String(i);
+			const table = 
+			`<div class="result-table">
+				<table style="width: 100%;">
+					<tr>
+						<th colspan="4" class=${config.idPrefix}>${config.title + " " + (i + 1)}</th>
+					</tr>
+					<tr>
+						<td class=${boxId + '00'}></td>
+						<td class=${boxId + '01'}></td>
+						<td class=${boxId + '02'}></td>
+						<td class=${boxId + '03'}></td>
+					</tr>
+					<tr>
+						<td class=${boxId + '10'}></td>
+						<td class=${boxId + '11'}></td>
+						<td class=${boxId + '12'}></td>
+						<td class=${boxId + '13'}></td>
+					</tr>
+					<tr>
+						<td class=${boxId + '20'}></td>
+						<td class=${boxId + '21'}></td>
+						<td class=${boxId + '22'}></td>
+						<td class=${boxId + '23'}></td>
+					</tr>
+					<tr>
+						<td class=${boxId + '30'}></td>
+						<td class=${boxId + '31'}></td>
+						<td class=${boxId + '32'}></td>
+						<td class=${boxId + '33'}></td>
+					</tr>
+				</table>
+				<div class=${'current-string-' + boxId}></div>
+			</div>`
+
+			allArea.innerHTML += table;
+			document.querySelector("#" + config.idPrefix + "-area").innerHTML += table;
+		})
+		allArea.innerHTML += '<hr />'
+	}
+}
+
+
+
---
+++ modules/cypher.js
@@ -0,0 +1,275 @@
+//Function that generates all subkeys, given the initial key and the number of subkeys to generate
+const generateSubkeys = (initialKey, n) => {
+	
+	let keysArray = new Array(n).fill([])
+	for (let i = 0; i < n; i++) {
+		let previousMatrix, array;
+		i == 0 ? previousMatrix = initialKey : previousMatrix = keysArray[i - 1]
+		
+		keysArray[i] = new Array(4).fill([])
+		array = getRotatedColumn(previousMatrix, 3)
+		const sboxArray = array.map(element => {
+						return sbox[parseInt(element, 16)]
+					})
+		const rconArray = rcon[i]
+		const previousMatrixFirstColumn = getColumn(previousMatrix, 0)
+		const firstRow = xorArrays(xorArrays(previousMatrixFirstColumn,sboxArray), rconArray)
+		keysArray[i] = setColumn(keysArray[i], firstRow, 0)
+		for (let j = 1; j < 4; j++) {
+			const previousMatrixColumn = getColumn(previousMatrix, j)
+			const newMatrixColumn = getColumn(keysArray[i], j - 1)
+			const newColumn = xorArrays(previousMatrixColumn, newMatrixColumn)
+			keysArray[i] = setColumn(keysArray[i],newColumn, j)
+		}
+	}
+	return keysArray
+}
+
+//Function that given two arrays applies XOR operation to every one of their elements
+const xorArrays = (array1, array2) => {
+	return array1.map((element, i) => {
+		let val = parseInt(element, 16) ^ parseInt(array2[i], 16)
+		return val.toString(16)
+	})
+}
+
+const setColumn = (matrix, column, n) => {
+	column.map( (element, i) => {
+		matrix[i][n] = element
+	})
+
+	return matrix
+} 
+
+const getRotatedColumn = (matrix, n) => {
+	let columnArray = getColumn(matrix, n)
+	columnArray.push(columnArray.shift())
+	return columnArray
+}
+
+const getColumn = (matrix, n) => {
+	let previousMatrix = [...matrix]
+	columnArray = []
+	previousMatrix.map((array) => {
+		columnArray.push(array[n])
+	})
+
+	return columnArray
+}
+
+const addRoundKey = (matrix, cypherMatrix) => {
+	let resultMatrix = [ [], [], [], []]
+	matrix.map((array, i) => {
+		array.map((element, j) => {
+			let val = parseInt(element, 16) ^ parseInt(cypherMatrix[i][j], 16)
+			resultMatrix[i][j] = val.toString(16) 
+		})
+	})
+
+	return resultMatrix
+}
+
+const subBytes = matrix => {
+	let resultMatrix = [ [], [], [], []]
+	matrix.map((array, i) => {
+		array.map((element, j) => {
+			resultMatrix[i][j] = sbox[parseInt(element, 16)] 
+		})
+	})
+
+	return resultMatrix
+}
+
+const shiftRows = matrix => {
+	let resultMatrix = matrix
+	matrix.map((array, i) => {
+		for (let j = 0; j < i; j++) {
+			array.push(array.shift())	
+		}
+	})
+
+	return resultMatrix
+}
+
+const mixColumns = matrix => {
+	let mtx = [
+	[ '2', '3', '1', '1'],
+	[ '1', '2', '3', '1'],
+	[ '1', '1', '2', '3'],
+	[ '3', '1', '1', '2']]
+
+	let resultMatrix = [ [], [], [], []]
+
+	for (let i = 0; i < 4; i++) {
+		array1 = getColumn(matrix, i)
+		for (let j = 0; j < 4; j++) {
+			array2 = mtx[j]
+			resultMatrix[j][i] = multiplyColumns(array1, array2).toString(16)
+		}
+	}
+
+	return resultMatrix
+}
+
+const multiplyColumns = ( array1, array2 ) => {
+	let result = array1.reduce((sum, val, index) => {
+		val = parseInt(val, 16)
+		switch (array2[index]) {
+			case '1':
+				return sum ^ val
+			case '2':
+				value = multiplyBy2(val)
+				return sum ^ value
+
+			case '3': 
+				value = multiplyBy2(val)
+				value = value ^ val
+				return sum ^ value
+
+		}
+	}, 0)
+	return result
+}
+
+const multiplyBy2 = (val) => {
+	value = val * 2
+	if ( val > 127) {
+		value = value.toString(2)
+		value = value.split("")
+		value.shift()
+		value = value.join("")
+		value = parseInt(value, 2)
+		return value ^ 27
+	}
+	return value 	
+}
+
+const initializeMatrix = (n) => {
+	let matrix = []
+	for (let i = 0; i < n; i++) {
+		matrix[i] = new Array()
+	}
+
+	return matrix
+}
+
+const generateMatrixFromStr = (str) => {
+	const charArray = str.split("")
+
+	const hexArray = charArray.map(element => element.charCodeAt(0).toString(16))
+
+	const column1 = hexArray.slice(0,4)
+	const column2 = hexArray.slice(4,8)
+	const column3 = hexArray.slice(8,12)
+	const column4 = hexArray.slice(12,16)
+
+	let matrix = [[],[],[],[]]
+	matrix = setColumn(matrix, column1, 0)
+	matrix = setColumn(matrix, column2, 1)
+	matrix = setColumn(matrix, column3, 2)
+	matrix = setColumn(matrix, column4, 3)
+
+	return matrix
+}
+
+const generateStrFromMatrix = matrix => {
+	let array = [... getColumn(matrix, 0), ... getColumn(matrix, 1), ... getColumn(matrix, 2), ... getColumn(matrix, 3)]
+	array = fixSingleValHex(array)
+	let result = array.join("")
+
+	return result 
+}
+
+const fixSingleValHex = array => {
+	return array.map((element) => {
+		if(element.length < 2) {
+			return "0" + element
+		}
+		return element
+	})
+}
+
+const printAllResults = (matrix, str) => {
+	for (let i = 0; i < 10; i++) {
+		matrix[i].map((array, j) => {
+			array.map((element, k) => {
+				let boxes = document.getElementsByClassName( str + i + j + k)
+				boxes = Array.from(boxes)
+				boxes.forEach(box => {
+					box.innerHTML = element
+				})
+			}) 
+		})
+		let currentStrPlaces = document.querySelectorAll('.current-string-' + str + i)
+		currentStrPlaces = Array.from(currentStrPlaces)
+		const currentStr = generateStrFromMatrix(matrix[i])
+		currentStrPlaces.map(place => {
+			place.innerHTML = currentStr
+		})
+	}
+}
+
+const displayResultButtons = () => {
+	resultButtons.forEach( resultButton => {
+		resultButton.style.display = 'inline-block';
+	})
+}
+
+var subkeysMatrixes, subBytesMatrixes, shiftedRowsMatrixes, columnMixedMatrixes, roundResultMatrixes;
+
+
+const cypher = ( str, cypherKey, rounds = 10) => {
+	// let cypherKeyMatrix = [
+	// 	['2B', '28', 'AB', '09'],
+	// 	['7E', 'AE', 'F7', 'CF'],
+	// 	['15', 'D2', '15', '4F'],
+	// 	['16', 'A6', '88', '3C']
+	// ]
+
+	// let strMatrix = [ 
+	// ['32', '88', '31', 'E0'], 
+	// ['43', '5A', '31', '37'], 
+	// ['F6', '30', '98', '07'], 
+	// ['A8', '8D', 'A2', '34']]
+
+	const strMatrix = generateMatrixFromStr(str)
+	const cypherKeyMatrix = generateMatrixFromStr(cypherKey)
+
+	subkeysMatrixes = generateSubkeys(cypherKeyMatrix, rounds)
+	subBytesMatrixes = initializeMatrix(rounds)
+	shiftedRowsMatrixes = initializeMatrix(rounds)
+	columnMixedMatrixes = initializeMatrix(rounds)
+	roundResultMatrixes = initializeMatrix(rounds)
+
+	
+
+
+	roundResultMatrixes[0] = addRoundKey(strMatrix, cypherKeyMatrix)
+	
+	for (let i = 0; i < rounds - 1; i++) {
+		subBytesMatrixes[i] = subBytes(roundResultMatrixes[i])
+		shiftedRowsMatrixes[i] = shiftRows(subBytesMatrixes[i])
+		columnMixedMatrixes[i] = mixColumns(shiftedRowsMatrixes[i])
+		roundResultMatrixes[i + 1] = addRoundKey( columnMixedMatrixes[i], subkeysMatrixes[i])
+	}
+	subBytesMatrixes[rounds - 1] = subBytes(roundResultMatrixes[rounds - 1])
+	shiftedRowsMatrixes[rounds - 1] = shiftRows(subBytesMatrixes[rounds - 1])
+	let finalMatrix = addRoundKey( shiftedRowsMatrixes[rounds - 1], subkeysMatrixes[rounds - 1])
+
+
+	
+	let result = generateStrFromMatrix(finalMatrix)
+
+	return {
+		strMatrix,
+		cypherKeyMatrix,
+		subkeysMatrixes,
+		subBytesMatrixes,
+		shiftedRowsMatrixes,
+		columnMixedMatrixes,
+		roundResultMatrixes,
+		finalMatrix,
+		result
+	}
+	
+}
